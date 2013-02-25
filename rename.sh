32e0bfb03d83d9b614cb9d82fb14ff1d8f2f5bb6
Ryan Wagner <rwagner@spyderbytedesign.com>
2013-02-25 12:12:25 -0500

    Fixed bug with variable name and also added folder rename based on theme name.

--- rename.sh
+++ rename.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo 'Please enter a name for the theme.'
 read NAME
+cd ../
+mv subthemeplus $NAME
+cd $NAME
 mv "RENAME.info" $NAME".info"
 cd sass
 mv "RENAME-alpha-default.scss" $NAME"-alpha-default.scss"
@@ -9,6 +12,6 @@ mv "RENAME-alpha-default-normal.scss" $NAME"-alpha-default-normal.scss"
 mv "RENAME-alpha-default-wide.scss" $NAME"-alpha-default-wide.scss"
 cd ..
 vim $NAME".info"
-drush en NAME
-drush vset theme_default NAME
+drush en $NAME
+drush vset theme_default $NAME
 echo "Rename complete! Enjoy!"
